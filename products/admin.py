4b10663477c81403dc4425e23f58eebd0b285426
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-06-22 13:54:36 +0000

    customized the admin panel

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,23 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=300)
     friendly_name = models.CharField(max_length=300, null=True, blank=True)
 
