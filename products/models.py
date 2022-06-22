88a8f88b63bc3181476d2f7f30e3eb06c331c310
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-06-22 13:36:33 +0000

    Added products models and fixtures

---
+++ products/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    friendly_name = models.CharField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def __str__(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=300, null=True, blank=True)
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    image_url = models.URLField(max_length=2000, blank=True, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
