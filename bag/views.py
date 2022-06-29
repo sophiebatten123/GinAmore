36fc6860cb448b4ea119c3a4cc1498ec18944281
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-06-29 15:47:47 +0000

    Added the toast functionality for all types

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -16,14 +16,14 @@ def add_to_bag(request, item_id):
     '''
     Add the quantity of a product to the shopping bag
     '''
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'Added {product.name} to your bag!')
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag!')
@@ -37,14 +37,16 @@ def adjust_bag(request, item_id):
     '''
     Adjust the quantity of items in the shopping bag
     '''
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'{product.name} removed from your shopping bag')
 
     request.session['bag'] = bag
 
@@ -55,13 +57,11 @@ def remove_from_bag(request, item_id):
     '''
     Adjust the quantity of items in the shopping bag
     '''
-    try:
-        bag = request.session.get('bag', {})
-        bag.pop(item_id)
+    product = get_object_or_404(Product, pk=item_id)
+    bag = request.session.get('bag', {})
+    bag.pop(item_id)
+    messages.success(request, f'{product.name} removed from your shopping bag')
 
-        request.session['bag'] = bag
+    request.session['bag'] = bag
 
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        return HttpResponse(status=500)
+    return HttpResponse(status=200)
