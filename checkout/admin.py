51f52e95383fdc4e155187cdde3f72362a10c99a
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-08 13:08:59 +0000

    checkout admin has been added

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number',
+                       'delivery_cost',
+                       'order_total',
+                       'grand_total',
+                       'date',)
+                       
+    fields = ('full_name',
+              'order_number',
+              'date',
+              'phone_number',
+              'country',
+              'email',
+              'postcode',
+              'town_or_city',
+              'street_address1',
+              'street_address2',
+              'county',
+              'delivery_cost',
+              'order_total',
+              'grand_total',)
+
+    list_display = ('full_name',
+                    'order_number',
+                    'date',
+                    'delivery_cost',
+                    'order_total',
+                    'grand_total',)
+
+    ordering = ('-date',)
