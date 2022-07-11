2668a4c50ad22b5e913a2973cae910287821dc9e
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-11 12:50:14 +0000

    stripe added to settings file

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
+from bag.contexts import bag_content
 from .forms import OrderForm
+
+import stripe
 
 
 def checkout(request):
@@ -9,6 +13,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "There is nothing in your bag!")
         return redirect(reverse('products'))
+
+    current_bag = bag_content(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
