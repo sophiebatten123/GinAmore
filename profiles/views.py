69c8e8057c639d30505437fa9fab00f7dac9d2a4
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-13 08:01:59 +0000

    Added profile app, template and view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def profile(request):
+    '''
+    Displays the users profile information
+    '''
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
