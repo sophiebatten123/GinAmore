9fcd727a346a8d8d27600213b0c7cc663b309032
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-13 08:31:47 +0000

    Updated the login allauth templates and profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 def profile(request):
     '''
     Displays the users profile information
     '''
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
