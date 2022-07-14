81f83a99d7b6e467ca7e64374b783901e2ac5992
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-14 15:12:28 +0000

    Remove image option has been added to the edit product

--- products/forms.py
+++ products/forms.py
@@ -9,7 +9,9 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image',
+                             required=False,
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,4 +20,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black'
+            field.widget.attrs['class'] = 'border-black rounded-0'
