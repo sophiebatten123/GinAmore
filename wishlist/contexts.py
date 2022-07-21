cb6b5c14fcbc783c4b5efc060bab4baf11eed355
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-21 13:34:48 +0000

    wishlist items are appearing on the wishlist page

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -1,11 +1,20 @@
-
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def wishlist_items(request):
 
+    wishlist_products = []
     wishlist = request.session.get('wishlist', {})
 
-    context = {
-        'wishlist': wishlist,
-    }
+    for item_id in wishlist.keys():
+        product = get_object_or_404(Product, pk=item_id)
+        wishlist_products.append({
+            'item_id': item_id,
+            'product': product,
+        })
 
+    context = {
+        'wishlist_products': wishlist_products,
+    }
+    
     return context
