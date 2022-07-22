90d73386c1ac0951b8af0bc9e11dd9b8fff41489
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-22 15:46:48 +0000

    Removed secret key and set debug

--- ginamore/settings.py
+++ ginamore/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!osyyul1nl#t70jduk&h73c94h_urtv^fi6h*=av^j*el7&s)l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ginamore.herokuapp.com', 'localhost']
 
