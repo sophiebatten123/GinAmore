4576ff20f387c8d86d35a176ea08ff91982fc720
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-22 15:05:12 +0000

    deployment to heroku app

--- ginamore/settings.py
+++ ginamore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!osyyul1nl#t70jduk&h73c94h_urtv^fi6h*=av^j*el7&s)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ginamore.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'ginamore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABAE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
