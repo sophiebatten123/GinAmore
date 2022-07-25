7cce2164f213e17b13e95c3e38fe0810a0cddb0e
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-25 15:03:30 +0000

    Update the order number

--- checkout/models.py
+++ checkout/models.py
@@ -20,7 +20,7 @@ class Order(models.Model):
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True,
                                      related_name='orders')
-    order_number = models.CharField(max_length=30, null=False, editable=False)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     email = models.EmailField(max_length=250, null=False, blank=False)
