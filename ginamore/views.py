0a6f5579c9b46c12e0e82eafca6ee67ded4e747d
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-07-26 12:23:12 +0000

    Custom 404 page

---
+++ ginamore/views.py
@@ -0,0 +1,11 @@
+'''
+Imports relevant django packages
+'''
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    '''
+    Error Handler 404 - Page Not Found
+    '''
+    return render(request, "errors/404.html", status=404)
