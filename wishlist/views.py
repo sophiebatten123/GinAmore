ba5c3add86724bc339a7d86e9cd004532b9cf3c2
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-08-04 05:51:49 +0000

    User authentication for the wishlist model is now required

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,7 @@
 '''
 Imports relevant django packages
 '''
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from products.models import Product
@@ -16,7 +16,6 @@ def view_wishlist(request):
     return render(request, 'wishlist/wishlist.html')
 
 
-@login_required
 def add_to_wishlist(request, item_id):
     '''
     Add an item to the wishlist
@@ -25,19 +24,27 @@ def add_to_wishlist(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     wishlist = request.session.get('wishlist', {})
 
-    if request.POST:
-        if product.likes.filter(id=request.user.id).exists():
-            product.likes.remove(request.user)
-            messages.success(
-                request, f'{product.name} has been removed from your wishlist'
-            )
-        else:
-            product.likes.add(request.user)
-            messages.success(
-                request, f'{product.name} to your wishlist'
+    if request.user.is_authenticated:
+        if request.POST:
+            if product.likes.filter(id=request.user.id).exists():
+                product.likes.remove(request.user)
+                messages.success(
+                    request,
+                    f'{product.name} has been removed from your wishlist'
                 )
-            wishlist[item_id] = product.name
+            else:
+                product.likes.add(request.user)
+                messages.success(
+                    request, f'{product.name} to your wishlist'
+                    )
+                wishlist[item_id] = product.name
 
         request.session['wishlist'] = wishlist
 
+        return redirect(redirect_url)
+    else:
+        messages.error(
+            request,
+            'You must be logged in to add an item to your wishlist'
+        )
         return redirect(redirect_url)
