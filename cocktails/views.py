0c18606483f003fb00ad54bd568afd21ef897ac8
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-08-08 15:23:00 +0000

    Cocktails can now be filtered by category

--- cocktails/views.py
+++ cocktails/views.py
@@ -2,7 +2,7 @@
 Imports relevant django packages
 '''
 from django.shortcuts import render, get_object_or_404
-from .models import Cocktail
+from .models import Cocktail, Category
 
 
 def cocktails(request):
@@ -10,9 +10,16 @@ def cocktails(request):
     A view to return the render all cocktail recipes
     '''
     cocktails = Cocktail.objects.all()
+    categories = None
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        cocktails = cocktails.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
 
     context = {
         'cocktails': cocktails,
+        'current_categories': categories,
     }
 
     return render(request, 'cocktails/cocktails.html', context)
