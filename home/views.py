864390d60c21bd7c74b8271ac979465699261e84
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-08-09 00:18:24 +0000

    Added the carousel items

--- home/views.py
+++ home/views.py
@@ -2,13 +2,20 @@
 Imports relevant django packages
 '''
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     '''
     A view to return the index page
     '''
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'home/index.html', context)
 
 
 def contact(request):
