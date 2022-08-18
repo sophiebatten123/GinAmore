70fb039b6b491283e22fad3a9f6e952142e6ccdb
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-08-18 17:23:01 +0000

    Cocktail Admin Form has been set up

---
+++ cocktails/forms.py
@@ -0,0 +1,26 @@
+'''
+Imports relevant django packages
+'''
+from django import forms
+from .models import Cocktail, Category
+
+
+class CocktailForm(forms.ModelForm):
+    '''
+    Form to display the cocktail information
+    '''
+    class Meta:
+        '''
+        Category fields generated within the form
+        '''
+        model = Cocktail
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
