4f9bcda94326ff9c1ac083e3a6cd70e442d2425d
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-08-18 18:45:14 +0000

    Edit functionality for admin on cocktails

--- cocktails/urls.py
+++ cocktails/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.cocktails, name='cocktails'),
     path('<int:product_id>/', views.cocktail_detail, name='cocktail_detail'),
     path('add/', views.add_cocktail, name='add_cocktail'),
+    path('edit/<int:product_id>/', views.edit_cocktail, name='edit_cocktail'),
 ]
--- cocktails/views.py
+++ cocktails/views.py
@@ -61,3 +61,29 @@ def add_cocktail(request):
     }
 
     return render(request, template, context)
+
+
+def edit_cocktail(request, product_id):
+    '''
+    Edit a cocktail from the store
+    '''
+    cocktail = get_object_or_404(Cocktail, pk=product_id)
+    if request.method == 'POST':
+        form = CocktailForm(request.POST, request.FILES, instance=cocktail)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated the cocktail!')
+            return redirect(reverse('cocktail_detail', args=[cocktail.id]))
+        else:
+            messages.error(request, 'Failed to edit the cocktail. Ensure the form is valid')
+    else:
+        form = CocktailForm(instance=cocktail)
+        messages.info(request, f'You are editing {cocktail.name}')
+
+    template = 'cocktails/edit_cocktail.html'
+    context = {
+        'form': form,
+        'cocktail': cocktail,
+    }
+
+    return render(request, template, context)
